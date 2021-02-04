6cee533e7f3873ecb5957a6a5877348049105d56
Sean Walton <sean@pillbug.zone>
2021-02-04 16:58:26 +0000

    James now controllable by the player

---
+++ Assets/Scripts/Behaviours/Player/CharacterController2D.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class CharacterController2D : MonoBehaviour
+{
+
+    [SerializeField] private float runSpeed;
+
+    private Rigidbody2D rigidbody;
+    private Vector2 myVelocity;
+    private Vector2 currentInputDirection;
+    
+    private void Awake()
+    {
+        rigidbody = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Move(currentInputDirection);
+    }
+
+    private void Move(Vector2 direction)
+    {
+        myVelocity = rigidbody.velocity;
+        myVelocity.x = direction.x * runSpeed;
+        rigidbody.velocity = myVelocity;
+    }
+
+    public void OnMove(InputValue input)
+    {
+        currentInputDirection = input.Get<Vector2>();
+    }
+}
