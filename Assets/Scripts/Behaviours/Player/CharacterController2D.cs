de134cea2ed241cbd2eec3f2eedb7747aebc5fe4
Sean Walton <sean@pillbug.zone>
2021-02-11 15:50:13 +0000

    We can now detect if James is on the ground or not

--- Assets/Scripts/Behaviours/Player/CharacterController2D.cs
+++ Assets/Scripts/Behaviours/Player/CharacterController2D.cs
@@ -10,18 +10,37 @@ public class CharacterController2D : MonoBehaviour
     [SerializeField] private float runSpeed;
     [SerializeField] private float jumpSpeed;
 
+    [SerializeField] private Transform groundCheckTr;
+    [SerializeField] private float groundCheckRadius;
+    [SerializeField] private LayerMask groundLayers;
+
     private Rigidbody2D rigidbody;
     private Vector2 myVelocity;
     private Vector2 currentInputDirection;
+    private bool onGround;
+    private int numColliders;
+    private Collider2D[] colliders = new Collider2D[10];
     
     private void Awake()
     {
         rigidbody = gameObject.GetComponent<Rigidbody2D>();
+        onGround = false;
     }
 
     private void Update()
     {
         Move(currentInputDirection);
+    }
+
+    private void FixedUpdate()
+    {
+        GroundCheck();
+    }
+
+    private void GroundCheck()
+    {
+        numColliders = Physics2D.OverlapCircleNonAlloc(groundCheckTr.position, groundCheckRadius, colliders, groundLayers);
+        onGround = (numColliders > 0);
     }
 
     private void Move(Vector2 direction)
