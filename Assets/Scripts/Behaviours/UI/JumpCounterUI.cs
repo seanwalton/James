01ba91e960709f47539f728b52f64a53e1787054
Sean Walton <sean@pillbug.zone>
2021-02-18 15:57:30 +0000

    Created jump counter UI prefab

---
+++ Assets/Scripts/Behaviours/UI/JumpCounterUI.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class JumpCounterUI : MonoBehaviour
+{
+    private TextMeshProUGUI myText;
+    private JumpCounter jumpCounter;
+
+    private int lastJumps;
+
+    private void Awake()
+    {
+        myText = GetComponent<TextMeshProUGUI>();
+        jumpCounter = FindObjectOfType<JumpCounter>();
+    }
+
+    private void Start()
+    {
+        UpdateJumpUI();
+    }
+
+    private void UpdateJumpUI()
+    {
+        lastJumps = jumpCounter.NumberOfJumps;
+        SetText(lastJumps);
+    }
+
+    private void Update()
+    {
+        if (lastJumps != jumpCounter.NumberOfJumps)
+        {
+            UpdateJumpUI();
+        }
+    }
+
+    private void SetText(int numJumps)
+    {
+        myText.text = numJumps.ToString() + " jumps";
+    }
+
+
+}
