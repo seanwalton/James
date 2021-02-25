b765ee50fb7b0172901c63161ad55623a72f3c3f
Sean Walton <sean@pillbug.zone>
2021-02-25 15:51:02 +0000

    Level 1 loads correctly from main menu

--- Assets/Scripts/Behaviours/Menu UI/MainMenuManager.cs
+++ Assets/Scripts/Behaviours/Menu UI/MainMenuManager.cs
@@ -1,16 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
     public void LaunchLevel1()
     {
-        Debug.Log("Launch level 1");
+        StartCoroutine(LoadLevel1Async());
     }
 
     public void ExitGame()
     {
         Application.Quit();
     }
+
+    IEnumerator LoadLevel1Async()
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("Level1");
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+
 }
