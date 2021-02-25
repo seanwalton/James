1b689a9c265d13ffc6c240b3130a67e978abe45b
Sean Walton <sean@pillbug.zone>
2021-02-25 15:38:39 +0000

    Added main menu scene and hooks from buttons to menu manager

---
+++ Assets/Scripts/Behaviours/Menu UI/MainMenuManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public void LaunchLevel1()
+    {
+        Debug.Log("Launch level 1");
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Exit Game");
+    }
+}
