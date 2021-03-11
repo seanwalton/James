f75f0fe4e301a213c40b4bf2d462b38035d7fac4
Sean Walton <sean@pillbug.zone>
2021-03-11 15:44:05 +0000

    Added sound effect API and data - plus a screaming james

---
+++ Assets/Scripts/Behaviours/Audio/SoundEffect.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SoundEffect", menuName = "ScriptableObjects/Sound Effect", order = 1)]
+public class SoundEffect : ScriptableObject
+{
+    [SerializeField] private AudioClip[] clip;
+    [SerializeField] private float volume;
+    [SerializeField] private float pitch;
+
+    public void PlayEffect(AudioSource source)
+    {
+        source.pitch = pitch;
+        source.PlayOneShot(clip[Random.Range(0, clip.Length)], volume);
+    }
+
+
+
+}
---
+++ Assets/Scripts/Behaviours/Screamers/ScreamingJamesBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class ScreamingJamesBehaviour : MonoBehaviour
+{
+    [SerializeField] private SoundEffect screamEffect;
+    private AudioSource myAudioSource;
+
+    private void Awake()
+    {
+        myAudioSource = GetComponent<AudioSource>();
+    }
+
+    private void Start()
+    {
+        InvokeRepeating("PlayEffect", 0f, 1f);
+    }
+
+
+    private void PlayEffect()
+    {
+        Debug.Log("Playing sound");
+        screamEffect.PlayEffect(myAudioSource);
+    }
+
+}
